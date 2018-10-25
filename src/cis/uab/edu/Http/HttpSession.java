e3b5662edcd648a34af552649cab5a159222e868
Hunter <hunt2013@me.com>
2018-10-25 15:09:48 -0500

    Added comments to HttpSession

--- src/cis/uab/edu/Http/HttpSession.java
+++ src/cis/uab/edu/Http/HttpSession.java
@@ -5,6 +5,10 @@ import java.net.Socket;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+/**
+ * Handles creation and use of HttpRequest, HttpHeaders, and HttpResponses.
+ * Keep previous items in memory and provides easy solution for sending multiple requests to the same address.
+ */
 public class HttpSession
 {
 	String address;
@@ -22,7 +26,12 @@ public class HttpSession
 	HttpRequest lastRequest = null;
 	HttpResponse lastResponse = null;
 	
-	
+	/**
+	 * Creates a Session with a header to start with
+	 * @param address Address to connect sockets to
+	 * @param port Port to connect sockets to
+	 * @param startHeader Starting HTTPHeader
+	 */
 	public HttpSession(String address, int port, HttpHeader startHeader)
 	{
 		this.address = address;
@@ -31,6 +40,12 @@ public class HttpSession
 		this.headerPath = startHeader.connectPath;
 	}
 	
+	/**
+	 *  Creates a session with a path to add to headers
+	 * @param address Address to connect sockets to
+	 * @param port Port to connect sockets to
+	 * @param connectionPath Path that is added to header to connect at
+	 */
 	public HttpSession(String address, int port, String connectionPath)
 	{
 		this.address = address;
@@ -38,6 +53,12 @@ public class HttpSession
 		this.headerPath = connectionPath;
 	}
 	
+	/**
+	 * Creates an HTTPRequest to use.
+	 * @param using HttpHeader to use in creation
+	 * @return Created HttpRequest
+	 * @throws IOException
+	 */
 	private HttpRequest createRequest(HttpHeader using) throws IOException
 	{
 		HttpRequest toReturn = new HttpRequest(new Socket(address, port), using, storedMatchers);
@@ -48,6 +69,11 @@ public class HttpSession
 		return toReturn;
 	}
 	
+	/**
+	 * Creates an array of headers depending on Http Method
+	 * @param method Method of which to get headers for
+	 * @return Combined Http Headers from the method specified
+	 */
 	private String[] createHeaderValues(HttpMethod method)
 	{
 		ArrayList<String> toReturn = new ArrayList<>(Arrays.asList(bothHeaders));
@@ -63,6 +89,10 @@ public class HttpSession
 		return toReturn.toArray(new String[]{});
 	}
 	
+	/**
+	 * Adds an HttpMatcher that is applied to all HttpRequests created
+	 * @param matcher HttpMatcher to add to Request
+	 */
 	public void addMatcher(HttpLineMatcher matcher)
 	{
 		ArrayList<HttpLineMatcher> temp = new ArrayList<>(Arrays.asList(storedMatchers));
@@ -70,6 +100,11 @@ public class HttpSession
 		storedMatchers = temp.toArray(storedMatchers);
 	}
 	
+	/**
+	 * Adds an Http Header to GET/POST requests
+	 * @param key header name
+	 * @param value header value
+	 */
 	public void addHeaderValue(String key, String value)
 	{
 		ArrayList<String> temp = new ArrayList<>(Arrays.asList(bothHeaders));
@@ -78,6 +113,12 @@ public class HttpSession
 		if (header != null) header.addHeader(key + ": " + value);
 	}
 	
+	/**
+	 * Adds and Http Header to either GET or POST requests
+	 * @param method Method to add to
+	 * @param key Header name
+	 * @param value Header value
+	 */
 	public void addMethodHeader(HttpMethod method, String key, String value)
 	{
 		ArrayList<String> headers;
@@ -98,21 +139,38 @@ public class HttpSession
 		}
 		if (this.header != null && this.header.method == method) this.header.addHeader(key + ": " + value);
 	}
+	
+	/**
+	 * Sets whether the request will print out its full HttpResponse to line once completed
+	 * @param bool
+	 */
 	public void printResponse(Boolean bool)
 	{
 		this.printWhenDone = bool;
 	}
 	
+	/**
+	 * Sets a specific Header to use on next send operation
+	 * @param newHeader HttpHeader to Set
+	 */
 	public void setHeader(HttpHeader newHeader)
 	{
 		this.header = newHeader;
 	}
 	
+	/**
+	 * Sets the path that the generated HttpHeader will use. Does not apply to manually passed header.
+	 * @param newPath
+	 */
 	public void setHeaderPath(String newPath)
 	{
 		this.headerPath = newPath;
 	}
 	
+	/**
+	 * Sends a GET HttpRequest with no Body
+	 * @throws IOException
+	 */
 	public void send() throws IOException
 	{
 		HttpHeader using = (header == null) ? new HttpHeader(HttpMethod.GET, headerPath, createHeaderValues(HttpMethod.GET)) : header;
@@ -120,6 +178,11 @@ public class HttpSession
 		this.lastResponse = new HttpResponse(this.lastRequest.getResponse());
 	}
 	
+	/**
+	 * Sends a POST HttpRequest with specified Body
+	 * @param body Body to send with
+	 * @throws IOException
+	 */
 	public void send(String body) throws IOException
 	{
 		HttpHeader using = (header == null) ? new HttpHeader(HttpMethod.POST, headerPath, createHeaderValues(HttpMethod.POST)) : header;
@@ -127,18 +190,36 @@ public class HttpSession
 		this.lastResponse = new HttpResponse(this.lastRequest.getResponse());
 	}
 	
+	/**
+	 * Sends HttpRequest with specified Header and body. Overwrites any stored header within the session.
+	 * @param header HttpHeader to send with
+	 * @param body Body to send with
+	 * @throws IOException
+	 */
 	public void send(HttpHeader header, String body) throws IOException
 	{
 		this.header = header;
 		send(body);
 	}
 	
+	/**
+	 *  Sends HttpRequest with specified HttpRequest. Overwrites any stored header within the session.
+	 * @param header HttpHeader to send with
+	 * @throws IOException
+	 */
 	public void send(HttpHeader header) throws IOException
 	{
 		this.header = header;
 		send();
 	}
 	
+	/**
+	 * Returns the Request that would normally be sent with a body. If body is empty the request is automatically GET
+	 * and POST if a body is provided. Uses stored HttpHeader if applied.
+	 * @param body Body to send with
+	 * @return String of HttpRequest that would be sent
+	 * @throws IOException
+	 */
 	public String getFullRequest(String body) throws IOException
 	{
 		HttpHeader using = (!(body != null && body.length() > 0))
@@ -150,6 +231,10 @@ public class HttpSession
 				: createRequest(header)).requestToString(body);
 	}
 	
+	/**
+	 * The last HttpResponse received.
+	 * @return Stored HttpResponse
+	 */
 	public HttpResponse getLastResponse()
 	{
 		return this.lastResponse;
